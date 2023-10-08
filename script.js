9203e0cf4b984fc0a287ce3eea50e69e79b3d735
Reynaldo Souza de Carvalho <122836752+ReySouza@users.noreply.github.com>
2023-10-08 20:23:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -64,51 +64,77 @@ document.addEventListener('DOMContentLoaded', function () {
         [[-33.018, 19.98], [-33.014, 21.242]]   // Image 9 bounds
     ];
 
-    function toggleImageOverlay(index) {
-        if (activeImageIndex === index) {
-            // If the same image is clicked again, remove it
-            if (imageOverlays[index]) {
-                map.removeLayer(imageOverlays[index]);
-                imageOverlays[index] = null;
-                activeImageIndex = -1;
-            }
-        } else {
-            // Remove the active image overlay (if any)
-            if (activeImageIndex !== -1 && imageOverlays[activeImageIndex]) {
-                map.removeLayer(imageOverlays[activeImageIndex]);
-                imageOverlays[activeImageIndex] = null;
-            }
+    const imageOverlays = imageUrls.map((imageUrl, index) => {
+                const bounds = imageBoundsArray[index];
+                const imageOverlay = L.imageOverlay(imageUrl, bounds).addTo(map);
 
-            // Add the clicked image overlay
-            if (!imageOverlays[index]) {
-                imageOverlays[index] = L.imageOverlay(imageUrls[index], imageBoundsArray[index]).addTo(map);
-                activeImageIndex = index;
-            }
-        }
-    }
-
-    for (let i = 0; i < imageUrls.length; i++) {
-        const button = L.Control.extend({
-            options: {
-                position: 'topleft'
-            },
-
-            onAdd: function () {
-                const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
-                container.innerHTML = `<button class="leaflet-control-button" data-index="${i}">Toggle Image ${i + 1}</button>`;
-                L.DomEvent.disableClickPropagation(container);
-
-                L.DomEvent.on(container, 'click', function (e) {
-                    const index = e.target.getAttribute('data-index');
-                    toggleImageOverlay(index);
+                // Add a click event listener to each image overlay
+                imageOverlay.on('click', function () {
+                    openSideMenu(index);
                 });
 
-                return container;
+                return imageOverlay;
+            });
+
+            // Function to open the side menu and display image details
+            function openSideMenu(imageIndex) {
+                const sideMenu = document.getElementById('side-menu');
+                const sideMenuContent = document.getElementById('side-menu-content');
+
+                // Add image details to the side menu content
+                sideMenuContent.innerHTML = `
+                    <h2>Image Details</h2>
+                    <p>Image ${imageIndex + 1} details go here.</p>
+                `;
+
+                // Open the side menu
+                sideMenu.classList.add('open');
             }
+
+            // Create and add buttons to toggle image overlays
+            for (let i = 0; i < imageUrls.length; i++) {
+                const button = L.Control.extend({
+                    options: {
+                        position: 'topleft'
+                    },
+
+                    onAdd: function () {
+                        const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+                        container.innerHTML = `<button class="leaflet-control-button" data-index="${i}">Toggle Image ${i + 1}</button>`;
+                        L.DomEvent.disableClickPropagation(container);
+
+                        L.DomEvent.on(container, 'click', function (e) {
+                            const index = e.target.getAttribute('data-index');
+                            toggleImageOverlay(index);
+                        });
+
+                        return container;
+                    }
+                });
+
+                map.addControl(new button());
+            }
+
+            // Function to toggle image overlays
+            function toggleImageOverlay(index) {
+                const imageOverlay = imageOverlays[index];
+                if (imageOverlay) {
+                    if (activeImageIndex === index) {
+                        // If the same image is clicked again, remove it
+                        map.removeLayer(imageOverlay);
+                        activeImageIndex = -1;
+                    } else {
+                        // Remove the active image overlay (if any)
+                        if (activeImageIndex !== -1) {
+                            map.removeLayer(imageOverlays[activeImageIndex]);
+                        }
+                        // Add the clicked image overlay
+                        imageOverlay.addTo(map);
+                        activeImageIndex = index;
+                        openSideMenu(index);
+                    }
+                }
+            }
+
+            let activeImageIndex = -1; // Variable to track the active image overlay
         });
-
-        map.addControl(new button());
-    }
-
-    const imageOverlays = new Array(imageUrls.length);
-});
