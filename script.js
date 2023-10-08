e7cc28a999ab412777c1d894c2b50b171e3351e4
Reynaldo Souza de Carvalho <122836752+ReySouza@users.noreply.github.com>
2023-10-08 18:16:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const map = L.map('map').setView([-29.0000, 24.0000], 5);
 
-    const kmlUrl = 'bioscape_domain_20220201.kml';
+    const streetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+    });
 
+    const satelliteMap = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        attribution: 'Tiles &copy; Google',
+        maxZoom: 20
+    });
+
+    const baseMaps = {
+        "Street Map": streetMap,
+        "Satellite Map": satelliteMap
+    };
+
+    L.control.layers(baseMaps).addTo(map);
+    streetMap.addTo(map);
+
+    const kmlUrl = 'bioscape_domain_20220201.kml';
     const kmlLayer = omnivore.kml(kmlUrl, {
         style: function (feature) {
             return {
