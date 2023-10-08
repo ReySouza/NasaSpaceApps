7369614f24a3b02c1f7a3b6ee908b7f80828198a
Reynaldo Souza de Carvalho <122836752+ReySouza@users.noreply.github.com>
2023-10-08 15:01:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-    var map = L.map('map').setView([-14.2350, -51.9253], 5);
+        var map = L.map('map').setView([-14.2350, -51.9253], 5);
 
-    var streetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-    });
+        var streetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+        });
 
-    var satelliteMap = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-      attribution: 'Tiles &copy; Google',
-      maxZoom: 20
-    });
+        var satelliteMap = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+            attribution: 'Tiles &copy; Google',
+            maxZoom: 20
+        });
 
-    var baseMaps = {
-      "Street Map": streetMap,
-      "Satellite Map": satelliteMap
-    };
+        var baseMaps = {
+            "Street Map": streetMap,
+            "Satellite Map": satelliteMap
+        };
 
-    L.control.layers(baseMaps).addTo(map);
-    streetMap.addTo(map);
+        L.control.layers(baseMaps).addTo(map);
+        streetMap.addTo(map);
