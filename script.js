075c96196448e0e04387aa3eb133beb3fe91b18c
Reynaldo Souza de Carvalho <122836752+ReySouza@users.noreply.github.com>
2023-10-08 17:00:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-        document.addEventListener('DOMContentLoaded', function () {
-            var map = L.map('map').setView([-29.0000, 24.0000], 5);
+document.addEventListener('DOMContentLoaded', function () {
+    const map = L.map('map').setView([-29.0000, 24.0000], 5);
 
-            var streetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-                attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-            });
+    const streetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+    });
 
-            var satelliteMap = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-                attribution: 'Tiles &copy; Google',
-                maxZoom: 20
-            });
+    const satelliteMap = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        attribution: 'Tiles &copy; Google',
+        maxZoom: 20
+    });
 
-            var baseMaps = {
-                "Street Map": streetMap,
-                "Satellite Map": satelliteMap
+    const baseMaps = {
+        "Street Map": streetMap,
+        "Satellite Map": satelliteMap
+    };
+
+    L.control.layers(baseMaps).addTo(map);
+    streetMap.addTo(map);
+
+    const kmlUrl = 'bioscape_domain_20220201.kml';
+    const kmlLayer = omnivore.kml(kmlUrl, {
+        style: function (feature) {
+            return {
+                fillColor: 'transparent',
+                color: 'blue',
+                weight: 2,
             };
+        }
+    });
 
-            L.control.layers(baseMaps).addTo(map);
-            streetMap.addTo(map);
+    kmlLayer.addTo(map);
 
-            var kmlUrl = 'bioscape_domain_20220201.kml';
-        var kmlLayer = omnivore.kml(kmlUrl);
-        kmlLayer.addTo(map);
+    kmlLayer.on('ready', function () {
+        map.fitBounds(kmlLayer.getBounds());
+    });
 
-        kmlLayer.on('ready', function () {
-            map.fitBounds(kmlLayer.getBounds());
-
-        });
-
-        });
+    kmlLayer.on('error', function (error) {
+        console.error('Error loading KML layer:', error);
+    });
+});
