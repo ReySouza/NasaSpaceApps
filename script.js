93306c997d99cd48fdfdbd1d555ef62a71658e9d
Reynaldo Souza de Carvalho <122836752+ReySouza@users.noreply.github.com>
2023-10-08 14:22:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    var map = L.map('map').setView([51.505, -0.09], 13); // Replace with your desired coordinates and zoom level
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-    }).addTo(map);
-});
 
